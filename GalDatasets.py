ae987d3e4f70a9ceaae97a5a04f7df5f263ec910
Sarah Jeffreson <sarah.jeffreson@cfa.harvard.edu>
2024-02-19 10:58:38 -0500

    first commit of the GalDatasets class for training data, and the training setup for flows. Not yet tested

--- GalDatasets.py
+++ GalDatasets.py
@@ -1,11 +1,240 @@
-# Like ETGDatasets.py but need the following functions:
-# Read all properties
-# Read particular properties in a list/keyword and for particular galaxies
-# Read properties in different forms (for now just do the continuous
-# list KS relation form, because we need to think about how to read the
-# data for spatial distribution, when galaxies are different sizes)
-# In fact maybe we don't need to train with a spatial distribution,
-# we can predict that just from the spatial distribution of the other
-# properties...? But we need to think about the use of LR and HR to
-# train and predict clumping factors.
-# Remember to cut all datasets to the same size in the above!
+"""
+    Class written by Carol Cuesta-Lazaro
+    and Sarah Jeffreson
+"""
+""" Class to load dataset from galaxy simulations and perform various
+    formatting operations
+"""
+
+from typing import List, Dict, Tuple
+from pathlib import Path
+import numpy as np
+import torch
+
+import astro_helper as ah
+
+DEFAULT_ROOT_DIR = Path("/n/holystore01/LABS/itc_lab/Lab/to-Carol/")
+class GalDataset:
+    """Dataset for general galaxy simulation snapshots of a general size"""
+    def __init__(
+        self,
+        input_features: List[str],
+        output_features: List[str],
+        galaxy_types: List[str],
+        root_dir: Path = DEFAULT_ROOT_DIR
+    ):
+        self.input_features = input_features
+        self.output_features = output_features
+        self.galaxy_types = galaxy_types
+        self.root_dir = root_dir
+
+        self.all_filenames_by_feature = self.get_all_filenames_by_feature()
+        self.min_num_usable_px = self.get_min_num_usable_px()
+
+    def read_property(
+        self,
+        snapshot: int,
+        galaxy_type: str,
+        feature: str
+    ) -> np.array:
+        """Read a property from a snapshot as a 2D array of input size,
+        including all data (all pixels) from that snapshot
+        Args:
+            snapshot (int): snapshot number
+            galaxy_type (str): type of galaxy
+            feature (str): feature to read
+        Returns:
+            np.array: image with feature
+        """
+        filename = f"{galaxy_type}/{feature}_{snapshot}.npy"
+        return np.load(self.root_dir / filename)
+    
+    def get_idcs_usable_px(
+        self,
+        galaxy_type: str
+    ) -> np.array:
+        """Get the indices of the pixels in a galaxy that are not zero
+        Args:
+            galaxy_type (str): type of galaxy
+        Returns:
+            np.array: indices of pixels that are not zero
+        """
+        density = np.array(ah.flatten_list([
+            list(np.ravel(np.load(item))) for item in self.get_filenames(
+                galaxy_type=galaxy_type,
+                feature="midplane_gas_dens")
+        ]))
+        return np.where(density>0)[0]
+
+    def get_num_usable_px(
+        self,
+        galaxy_type: str
+    ) -> int:
+        """Get the number of pixels in a galaxy that are not zero
+        Args:
+            galaxy_type (str): type of galaxy
+        Returns:
+            int: number of pixels that are not zero
+        """
+        idcs_usable_px = self.get_idcs_usable_px(galaxy_type)
+        return len(idcs_usable_px)
+
+    def get_min_num_usable_px(
+        self,
+    ) -> int:
+        """Get the minimum number of pixels across all galaxies
+        Returns:
+            int: minimum number of pixels across all galaxies
+        """
+        min_num_px = np.inf
+        for galaxy_type in self.galaxy_types:
+            num_px = self.get_num_usable_px(galaxy_type)
+            if num_px < min_num_px:
+                min_num_px = num_px
+        return min_num_px
+
+    def __getitem__(
+        self,
+        idx: int
+    ):
+        """Get full (all pixels) 2D array at index idx, across all galaxy types,
+        by feature.
+        Args:
+            idx (int): index to get
+        Returns:
+            Tuple[Tensor, Tensor]: input and output images
+        """
+        input_features = [
+            torch.tensor(np.load(self.all_filenames_by_feature[feature][idx]))
+            for feature in self.input_features
+        ]
+        output_features = [
+            torch.tensor(np.load(self.all_filenames_by_feature[feature][idx]))
+            for feature in self.output_features
+        ]
+        return input_features, output_features
+    
+    def __len__(
+        self,
+    ) -> int:
+        return len(self.all_filenames_by_feature[self.input_features[0]])
+    
+    def get_filenames(
+        self,
+        galaxy_type: str,
+        feature: str
+    ) -> List[str]:
+        """filenames in data folder
+        Returns:
+            List[str]: list of filenames in data folder
+        """
+        filename = f"{galaxy_type}/{feature}*_???.npy"
+        return list(sorted(self.root_dir.rglob(filename)))
+
+    def get_all_filenames(
+        self,
+        feature: str,
+    ):
+        filenames = []
+        for galaxy_type in self.galaxy_types:
+            filenames += self.get_filenames(
+                galaxy_type=galaxy_type, feature=feature,
+            )
+        return filenames
+
+    def get_filenames_density(
+        self,
+        galaxy_type: str,
+    ) -> List[str]:
+        """filenames for the mid-plane density of gas in data folder
+        Returns:
+            List[str]: list of filenames in data folder
+        """
+        filename = f"{galaxy_type}/midplane_gas_dens_*.npy"
+        return list(sorted(self.root_dir.rglob(filename)))
+
+    def get_all_filenames_density(
+        self,
+    ) -> List[str]:
+        filenames = []
+        for galaxy_type in self.galaxy_types:
+            filenames += self.get_filenames_density(galaxy_type)
+        return filenames
+
+   def get_all_filenames_by_feature(
+        self,
+    ):
+        all_filenames_by_feature = {}
+        for feature in self.input_features:
+            all_filenames_by_feature[feature] = self.get_all_filenames(
+                feature=feature
+            )
+        for feature in self.output_features:
+            all_filenames_by_feature[feature] = self.get_all_filenames(
+                feature=feature
+            )
+        return all_filenames_by_feature
+
+    def get_filenames_by_feature(
+        self,
+        galaxy_type: str,
+    ) -> Dict[str, List[str]]:
+        filenames_by_feature = {}
+        for feature in self.input_features:
+            filenames_by_feature[feature] = self.get_filenames(
+                galaxy_type=galaxy_type,
+                feature=feature
+            )
+        for feature in self.output_features:
+            filenames_by_feature[feature] = self.get_filenames(
+                galaxy_type=galaxy_type,
+                feature=feature
+            )
+        return filenames_by_feature
+
+    def get_data_as_1d(
+        self
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Get data for a feature as a 1D array, for a particular
+        galaxy type. Sample only the number of pixels if specified,
+        generally equal to the smallest number of pixels of any
+        galaxy in the sample (so that training samples are of equal
+        size). Assumes same amount of data for each feature.
+        Args:
+            feature: feature to analyze
+            galaxy_type: self-explanatory
+            num_px: number of pixels to sample (across all snapshots)
+        Returns:
+            Tuple[Tensor, Tensor]: input and output arrays
+        """
+
+        input_features = {feature: [] for feature in self.input_features}
+        output_features = {feature: [] for feature in self.output_features}
+
+        for galaxy_type in self.galaxy_types:
+            nonzero_density_idcs = self.get_idcs_usable_px(galaxy_type)
+            px_idcs = np.random.choice(
+                nonzero_density_idcs,
+                size=self.min_num_usable_px,
+                replace=False
+            )
+
+            filenames_by_feature = self.get_filenames_by_feature(galaxy_type)
+            for feature in self.input_features:
+                input_features[feature].append(ah.flatten_list([
+                    list(np.ravel(np.load(filenames_by_feature[feature][idx])))
+                    for idx in len(filenames_by_feature[feature])
+                ]))
+
+            for feature in self.output_features:
+                output_features[feature].append(ah.flatten_list([
+                    list(np.ravel(np.load(filenames_by_feature[feature][idx])))
+                    for idx in len(filenames_by_feature[feature])
+                ]))
+
+        for feature in self.input_features:
+            input_features[feature] = torch.tensor(ah.flatten_list(input_features[feature]))
+        for feature in self.output_features:
+            output_features[feature] = torch.tensor(ah.flatten_list(output_features[feature]))
+        
+        return input_features, output_features
---
+++ train_flow.py
@@ -0,0 +1,181 @@
+"""
+    Class written by Carol Cuesta-Lazaro
+    and Sarah Jeffreson
+"""
+""" Class to train normalizing flows on galaxy simulation data, in order to
+    predict probability distributions of the star formation rate (surface
+    density or volume density)
+"""
+
+import sys
+import pickle
+import json
+import numpy as np
+from pathlib import Path
+
+import astro_helper as ah
+from GalDatasets import GalDataset
+
+import torch
+from torch import optim
+from nflows.flows.base import Flow
+from nflows.distributions.normal import StandardNormal
+from nflows.transforms.base import CompositeTransform
+from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
+from nflows.transforms.permutations import ReversePermutation
+
+DEFAULT_ROOT_DIR = Path("/n/holystore01/LABS/itc_lab/Lab/to-Carol/")
+def get_data(input_features, log_features, output_features, galaxy_types):
+    gals = GalDataset(
+        input_features=input_features,
+        output_features=output_features,
+        galaxy_types=galaxy_types
+
+    )
+    print("Number of galaxies = "+str(len(gal)))
+    input_dict, outputs_dict = gals.get_data_as_1d()
+
+    inputs = np.stack([
+        np.log10(input_dict[feature]) if feature in log_features else input_dict[feature]
+        for feature in input_features
+    ], axis=1)
+    outputs = np.stack([
+        np.log10(outputs_dict[feature]) if feature in log_features else outputs_dict[feature]
+        for feature in output_features
+    ], axis=1)
+
+    return inputs, outputs
+
+def standarize_data(logX, logY):
+    norm_dict = {
+        'x_min': np.min(logX, axis=0),
+        'y_min': np.min(logY, axis=0),
+    }
+
+    norm_dict['x_mean'] =  np.mean(logX, axis=0)
+    norm_dict['x_std'] =  np.std(logX, axis=0)
+
+    norm_dict['y_mean'] =  np.mean(logY, axis=0)
+    norm_dict['y_std'] =  np.std(logY, axis=0)
+
+    logX = (logX - norm_dict['x_mean']) / norm_dict['x_std']
+    logY = (logY - norm_dict['y_mean']) / norm_dict['y_std']
+
+    return logX, logY, norm_dict
+
+def get_flow(num_features, num_context_features, num_hidden_features, num_layers=6):
+    # Define the conditional normalizing flow
+    base_dist = StandardNormal(shape=[1])
+
+    transforms = []
+    for _ in range(num_layers):
+        transforms.append(MaskedAffineAutoregressiveTransform(
+            features=num_features, 
+            context_features=num_context_features,
+            hidden_features=num_hidden_features,
+            )
+        )
+        transforms.append(ReversePermutation(features=num_features))
+
+    transform = CompositeTransform(transforms)
+
+    flow = Flow(transform, base_dist)
+    return flow
+
+if __name__ == '__main__':
+    output_path = DEFAULT_ROOT_DIR / "flows"
+    target_galaxy = sys.argv[1]
+    galaxy_types = ['ETG-vlowmass', 'ETG-lowmass', 'ETG-medmass', 'ETG-himass', 'NGC300', 'MW']
+    galaxy_types.remove(target_galaxy)
+    galaxy_types = sorted(galaxy_types)
+
+    log_features = ['gas_surfdens', 'stellar_surfdens', 'weights', 'kappas']
+
+    '''training features that are broadly reliable in cosmo sims'''
+    if(sys.argv[2]=='0'):
+        input_features = sorted(['gas_surfdens'])
+    if(sys.argv[2]=='a'):
+        input_features = sorted(['gas_surfdens', 'stellar_surfdens'])
+    elif(sys.argv[2]=='b'):
+        input_features = sorted(['gas_surfdens', 'stellar_surfdens', 'weights'])
+    elif(sys.argv[2]=='c'):
+        input_features = sorted(['gas_surfdens', 'stellar_surfdens', 'weights', 'kappas'])
+
+    '''features that are debatably reliable in cosmo sims'''
+    elif(sys.argv[2]=='d'):
+        input_features = sorted(['gas_surfdens', 'stellar_surfdens', 'weights', 'kappa',
+        'midplane-dens', 'midplane-stellar-dens'])
+    elif(sys.argv[2]=='e'):
+        input_features = sorted(['gas_surfdens', 'stellar_surfdens', 'weights', 'kappa',
+        'midplane-dens', 'midplane-stellar-dens', 'midplane-Pturb', 'midplane-Pth'])
+    else:
+        input_features = sorted(['gas_surfdens', 'stellar_surfdens', 'weights', 'kappa',
+        'midplane-dens', 'midplane-stellar-dens', 'midplane-Pturb', 'midplane-Pth', 'midplane-veldispz', 'midplane-veldisp3D'])
+    output_path = output_path / target_galaxy
+    output_path = output_path / ("set_"+sys.argv[2])
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    num_hidden_features = 128
+    num_epochs = 1000 
+    batch_size = 256 
+    num_layers = 6
+
+    X, Y = get_data(
+        input_features=input_features,
+        log_features=log_features,
+        output_features=['SFR_surfdens'],
+        galaxy_types=galaxy_types,
+    )
+    print(type(X), type(Y), np.shape(X), np.shape(Y))
+    X, Y, norm_dict = standarize_data(X, Y, log_features_idx)
+
+    # store the flow
+    norm_dict = {k: v.tolist() for k, v in norm_dict.items()}
+    with open(output_path / 'norm_dict.json', 'w') as f:
+        json.dump(norm_dict, f)
+
+    print('X shape = ', X.shape)
+    print('Y shape = ', Y.shape)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('device = ', device)
+    X = torch.tensor(X, dtype=torch.float32).to(device)
+    Y = torch.tensor(Y, dtype=torch.float32).to(device)
+    flow = get_flow(
+        num_features = Y.shape[-1],
+        num_context_features = X.shape[-1],
+        num_hidden_features=num_hidden_features,
+        num_layers=num_layers,
+        
+    )
+    flow = flow.to(device)
+
+    # Train the flow
+    optimizer = optim.Adam(flow.parameters(), lr=1e-4)
+    for epoch in range(num_epochs):
+        permutation = torch.randperm(X.size(0))
+        X = X[permutation]
+        Y = Y[permutation]
+        for batch_start in range(0, X.size(0), batch_size):
+            batch_end = batch_start + batch_size
+            x_batch = X[batch_start:batch_end]
+            y_batch = Y[batch_start:batch_end]
+
+            optimizer.zero_grad()
+            log_prob = flow.log_prob(inputs=y_batch, context=x_batch)
+            loss = -log_prob.mean()
+            loss.backward()
+            optimizer.step()
+
+        if epoch % 10 == 0:
+            print(f'Epoch: {epoch}, Loss: {loss.item()}')
+        if epoch % 100 == 0:
+            loss_value = loss.item()
+            flow = flow.to('cpu')
+            with open(output_path / f'flow_e{epoch}_{loss_value:.4f}.pkl', 'wb') as f:
+                pickle.dump(flow, f)
+            flow = flow.to(device)
+
+        loss_value = loss.item()
+        flow_cpu = flow.to('cpu')
+        with open(output_path / f'flow_e{epoch}_{loss_value:.4f}.pkl', 'wb') as f:
+            pickle.dump(flow_cpu, f)
